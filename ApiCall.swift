a95fbe682abb04a987ea49bbbe4fb5485b74f4cb
priyanka <priyanka.bagul@brainvire.com>
2023-10-12 19:14:19 +0530

    first commit

---
+++ ApiCall.swift
@@ -0,0 +1,8 @@
+//
+//  ApiCall.swift
+//  TaskDemo
+//
+//  Created by Priyanka Bagul on 12/10/23.
+//
+
+import Foundation
---
+++ TaskDemo/userResueCell.swift
@@ -0,0 +1,17 @@
+//
+//  userResueCell.swift
+//  TaskDemo
+//
+//  Created by Priyanka Bagul on 12/10/23.
+//
+
+import UIKit
+
+class userResueCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
